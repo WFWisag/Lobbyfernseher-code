89c18c5b669ebaf551246c5834fb8b1c1b13fdf2
Michael Vorogushyn <michael.vorogushyn@wbgym.de>
2023-03-03 23:07:17 +0100

    Revert "stop working on implementation of videos"
    This reverts commit bdf529c12889ee889eb0d3d833b45d3bfc1a275e.

--- src/writeInHTML.py
+++ src/writeInHTML.py
@@ -55,7 +55,7 @@ def writeIMGinHTML(element, replacedTag):
         strSoup = str(soup)
 
     newhtmlfileRT = strSoup.replace(
-        replacedTag, f"<img class='folie' src='media/{element}' />")
+        oldTags, f"<img class='folie' src='media/{element}' />")
 
     with open("index.html", "w+") as newhtmlfile:
         newhtmlfile.write(newhtmlfileRT)
@@ -71,7 +71,7 @@ def writeVIDinHTML(element, replacedTag):
     writeTimeControlInHTML(clipDuration)
 
     newhtmlfileRT = strSoup.replace(
-        replacedTag, f"<video class='folie' src='media/{element}' autoplay></video>")
+        oldTags, f"<video class='folie' src='media/{element}' autoplay></video>")
 
     with open("index.html", "w+") as newhtmlfile:
         newhtmlfile.write(newhtmlfileRT)
