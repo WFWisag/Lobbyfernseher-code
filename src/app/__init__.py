7bc43662bbf26b6f3f4b07c7f5f2bdc94147c29e
micha003 <85707089+micha003@users.noreply.github.com>
2023-07-09 20:52:24 +0200

    Update __init__.py

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from . import views
 
+with open('SECRETKEY.txt') as secretkey:
+    sk = secretkey
+
 
 def create_app():
     app = Flask("app")
-    app.config["SECRET_KEY"] = "b'P\x98\x91\xec\xc2?\xee\x9d\xe9\xb8W\x1a'"
+    app.config["SECRET_KEY"] = sk
 
     app.register_blueprint(views.views, url_prefix="/")
     return app
